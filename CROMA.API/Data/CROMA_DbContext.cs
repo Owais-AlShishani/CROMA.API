0c6cf8348689487e09f4d74a0b8b843f9d93a176
Owais-AlShishani <98020718+Owais-AlShishani@users.noreply.github.com>
2022-10-04 18:56:55 +0300

    Created Initial Migration and Added Seeding Data

--- CROMA.API/Data/CROMA_DbContext.cs
+++ CROMA.API/Data/CROMA_DbContext.cs
@@ -11,5 +11,19 @@ namespace CROMA.API.Data
         }
         public DbSet<Order> Orders { get; set; }
         public DbSet<Car> Cars { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Car>().HasData(
+                new Car { Id = 1, CarType = "BMW" });
+            modelBuilder.Entity<Car>().HasData(
+                new Car { Id = 2, CarType = "KIA" });
+            modelBuilder.Entity<Car>().HasData(
+                new Car { Id = 3, CarType = "TOYOTA" });
+            modelBuilder.Entity<Car>().HasData(
+                new Car { Id = 4, CarType = "SUBARU" });
+        }
     }
 }
---
+++ CROMA.API/Migrations/20221004155339_Initial Migration.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CROMA.API.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cars",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CarType = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cars", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DateFrom = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DateTo = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNumber = table.Column<int>(type: "int", nullable: false),
+                    Comment = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CarId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Orders_Cars_CarId",
+                        column: x => x.CarId,
+                        principalTable: "Cars",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Cars",
+                columns: new[] { "Id", "CarType" },
+                values: new object[,]
+                {
+                    { 1, "BMW" },
+                    { 2, "KIA" },
+                    { 3, "TOYOTA" },
+                    { 4, "SUBARU" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_CarId",
+                table: "Orders",
+                column: "CarId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "Cars");
+        }
+    }
+}
