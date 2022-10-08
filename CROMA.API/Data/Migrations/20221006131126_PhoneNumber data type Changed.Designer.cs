f0de7761702e886ee76e987e0facb8c540ef1aa6
Owais-AlShishani <98020718+Owais-AlShishani@users.noreply.github.com>
2022-10-08 15:57:44 +0300

    Enbaled Cors

--- CROMA.API/Data/Migrations/20221006131126_PhoneNumber data type Changed.Designer.cs
+++ CROMA.API/Data/Migrations/20221006131126_PhoneNumber data type Changed.Designer.cs
@@ -4,6 +4,7 @@ using CROMA.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CROMA.API.Migrations
 {
     [DbContext(typeof(CROMA_DbContext))]
-    partial class CROMA_DbContextModelSnapshot : ModelSnapshot
+    [Migration("20221006131126_PhoneNumber data type Changed")]
+    partial class PhoneNumberdatatypeChanged
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CROMA.API/Data/Migrations/20221006131126_PhoneNumber data type Changed.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CROMA.API.Migrations
+{
+    public partial class PhoneNumberdatatypeChanged : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "PhoneNumber",
+                table: "Orders",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "PhoneNumber",
+                table: "Orders",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+        }
+    }
+}
